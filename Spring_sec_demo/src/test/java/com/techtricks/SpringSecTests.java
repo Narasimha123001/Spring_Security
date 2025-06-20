d3860143780a7c9d5daa40cba5706563388b3f2d
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-06-21 01:53:59 +0530

    "add: Added SpringSecTests for application context loading test"

---
+++ Spring_sec_demo/src/test/java/com/techtricks/SpringSecTests.java
@@ -0,0 +1,13 @@
+package com.techtricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
