150232ef4415a06376744c0ce9a27e0b3b6fe837
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-06-21 01:51:21 +0530

    "add:Added main file to run Spring project"

---
+++ Spring_sec_demo/src/main/java/com/techtricks/Spring_sec.java
@@ -0,0 +1,13 @@
+package com.techtricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_sec {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring_sec.class, args);
+    }
+
+}
